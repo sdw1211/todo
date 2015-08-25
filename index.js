b22fe327c46763f8d22152579e3ca42bd6f7d8d6
sdw1211 <sdw1211@gmail.com>
2015-08-25 13:25:38 +0900

    80포트로 변경

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", function(req, res) {
 	res.render("todo");
 });
 
-var server = app.listen("3000", function() {
+var server = app.listen("80", function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
